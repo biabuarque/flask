88db18fa9f51fe0c5e20816c1d85b774fbff6223
biabuarque <a257241@dac.unicamp.br>
2024-06-27 23:34:10 -0300

    feat: login, index & signup pages
    TODO 1: optimize alignment

--- blogy.py
+++ blogy.py
@@ -7,8 +7,20 @@ def home():
     return render_template('index.html', css_template='index.css')
 
 @app.route('/pomodoro')
-def blog():
+def pomodoro():
     return render_template('pomodoro.html', css_template='blog.css')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/forgotmypassword')
+def forgotmypassword():
+    return render_template('forgotmypassword.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
